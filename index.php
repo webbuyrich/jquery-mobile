30b2c72c187d351ae5b47bf17006237ec94ae79f
Richard Peterson <webbuyrich@gmail.com>
2015-12-17 16:17:59 -0600

    added buttons with icons

--- index.php
+++ index.php
@@ -18,50 +18,50 @@
     </head>
     <body>
 
-    	<section data-role="page" id="home">
-    		
-    		<div data-role="header">
-				header
-    		</div><!-- header -->
-    		
-    		<article data-role="content">
-    			<img src="css/images/logo_home.png"  id="logo-home" alt="">
-    			<a href="#" class="ui-btn ui-corner-all">Blog</a>
-    			<a href="#" class="ui-btn ui-corner-all">Videos</a>
-    			<a href="#" class="ui-btn ui-corner-all">Photos</a>
-    			<a href="#" class="ui-btn ui-corner-all">Sources</a>
+        <section data-role="page" id="home">
+            
+            <div data-role="header">
+                header
+            </div><!-- header -->
+            
+            <article data-role="content">
+                <img src="css/images/logo_home.png"  id="logo-home" alt="">
+                <a href="#" class="ui-btn ui-corner-all ui-btn-b ui-icon-bars ui-btn-icon-left">Blog</a>
+                <a href="#" class="ui-btn ui-corner-all ui-btn-b ui-icon-video ui-btn-icon-left">Videos</a>
+                <a href="#" class="ui-btn ui-corner-all ui-btn-b ui-icon-camera ui-btn-icon-left">Photos</a>
+                <a href="#" class="ui-btn ui-corner-all ui-btn-b ui-icon-cloud ui-btn-icon-left">Sources</a>
 
-    			
-				<h1>Welcome</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia assumenda aperiam, consequuntur, quia at optio accusamus quidem voluptatem explicabo minus. Enim rerum cumque quasi deserunt reprehenderit veritatis ad animi tempore!</p>
-				<a href="#info">Info</a>
-    		</article><!-- content -->
-    		
-    		<div data-role="footer">
-				footer
-    		</div><!-- footer -->
-    	
-    	</section>	<!-- Home Section -->
+                
+                <h1>Welcome</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia assumenda aperiam, consequuntur, quia at optio accusamus quidem voluptatem explicabo minus. Enim rerum cumque quasi deserunt reprehenderit veritatis ad animi tempore!</p>
+                <a href="#info">Info</a>
+            </article><!-- content -->
+            
+            <div data-role="footer">
+                footer
+            </div><!-- footer -->
+        
+        </section>  <!-- Home Section -->
 
-    	<section data-role="page" id="info">
-    		
-    		<div data-role="header">
-				header
-    		</div><!-- header -->
-    		
-    		<article data-role="content">
-					<h1>Welcome</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia assumenda aperiam, consequuntur, quia at optio accusamus quidem voluptatem explicabo minus. Enim rerum cumque quasi deserunt reprehenderit veritatis ad animi tempore!</p>
-					<a href="#home">Home</a>
-    		</article><!-- content -->
-    		
-    		<div data-role="footer">
-				footer
-    		</div><!-- footer -->
-    	
-    	</section>	<!-- Home Section -->
+        <section data-role="page" id="info">
+            
+            <div data-role="header">
+                header
+            </div><!-- header -->
+            
+            <article data-role="content">
+                    <h1>Welcome</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia assumenda aperiam, consequuntur, quia at optio accusamus quidem voluptatem explicabo minus. Enim rerum cumque quasi deserunt reprehenderit veritatis ad animi tempore!</p>
+                    <a href="#home">Home</a>
+            </article><!-- content -->
+            
+            <div data-role="footer">
+                footer
+            </div><!-- footer -->
+        
+        </section>  <!-- Home Section -->
 
-    	</div>
+        </div>
         <h1>JQuery Mobile</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem at consequuntur numquam impedit accusamus vero autem exercitationem fugit magni unde ea excepturi ipsa, explicabo praesentium delectus eaque tenetur omnis nobis.</p>
         
