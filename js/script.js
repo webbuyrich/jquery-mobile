ace1aeb493a9f701ed7a2458449eec607e09479f
Richard Peterson <webbuyrich@gmail.com>
2015-12-19 20:59:09 -0600

    updated json code. fixed users page

--- js/script.js
+++ js/script.js
@@ -5,18 +5,36 @@ $(document).ready(function (){
 		console.log(data);
 	});
 	*/
+	$('.link-users').click(function(){
+		usersList();
+	});
+	
+	function usersList(){
+		$.ajax({
+			url: "http://api.randomuser.me/?results=10",
+			dataType: 'json',
+			success: function(data){
+				$.each(data.results, function(i, user){
+					var output ='<li class="user-list">';
+						output+='<a href="#">';
+						output+='<img src="'+user.user.picture.thumbnail+'">';
+						output+='<h2>'+user.user.name.first+ ' '+user.user.name.last+'</h2>';
+						output+='<p>'+user.user.location.street+'<br />';
+						output+= user.user.location.city+','+user.user.location.state+'<br />';
+						output+= user.user.phone+'</p>';
+						output+='</a>';
+						output+='</li>';
+					$('#showUserList').append(output);
+				});			
+				
+			}
 
-	$.ajax({
-		url: 'http://api.randomuser.me/?results=10',
-		dataType: 'json',
-		type: 'get',
-		cache: false,
-		success: function(data){
-			$(data.results).each(function(index, value){
-				console.log(value.user.picture.medium);
-			})
-		}
-	})
+		})
+	}
+	
 
 
 });
+
+
+
