22ecbe7a14c27410cb46c65905523f85fdfc4d8d
Richard Peterson <webbuyrich@gmail.com>
2015-12-21 11:05:22 -0600

    added userlist with arrows and user information is next to picture

--- js/script.js
+++ js/script.js
@@ -7,29 +7,29 @@ $(document).ready(function (){
 	*/
 	$('.link-users').click(function(){
 		usersList();
+
 	});
 	
-	function usersList(){
+	function usersList(data){
 		$.ajax({
 			url: "http://api.randomuser.me/?results=10",
 			dataType: 'json',
 			success: function(data){
 				$.each(data.results, function(i, user){
-					var output ='<li class="user-list">';
-						output+='<a href="#">';
-						output+='<img src="'+user.user.picture.thumbnail+'">';
-						output+='<h2>'+user.user.name.first+ ' '+user.user.name.last+'</h2>';
-						output+='<p>'+user.user.location.street+'<br />';
+					var output ='<li class="ui-grid-a"><a href="#" class="ui-btn ui-btn-icon-right ui-icon-carat-r">';						
+						output+='<div class="ui-block-a"><img src="'+user.user.picture.thumbnail+'"></div>';
+						output+='<div class="ui-block-b user-list-text-padding"><p><span class="username-capitalize">'+user.user.name.first+ ' ' +user.user.name.last+'</span><br />';
+						output+= user.user.location.street+'<br />';
 						output+= user.user.location.city+','+user.user.location.state+'<br />';
-						output+= user.user.phone+'</p>';
+						output+= user.user.phone+'</p></div>';
 						output+='</a>';
 						output+='</li>';
-					$('#showUserList').append(output);
+						$('#user-list').append(output);
 				});			
 				
 			}
 
-		})
+		});
 	}
 	
 
